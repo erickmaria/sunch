53b1f323e82e0de47636027b0586ff43a59d35a4
Erick Rodrigues <erick.rodrigues@sitemercado.com.br>
2024-04-28 03:52:03 -0300

    feat: add app lock

--- electron/main.ts
+++ electron/main.ts
@@ -4,40 +4,49 @@ import { Window } from './ui/window';
 import { Tray } from './ui/tray';
 import { Shortcuts } from './helpers/shortcuts';
 
-const App = () => {
-  Window.getInstance()
-  Tray.getInstance()
-}
+const data = { lock: 'app.lock' }
+const gotTheLock = app.requestSingleInstanceLock(data)
 
-app.on('will-quit', () => {
-  Shortcuts.unregister()
-})
+if (!gotTheLock) {
+  app.quit()
+} else {
 
-app.disableHardwareAcceleration()
-
-app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
+  const App = () => {
     Window.getInstance()
-  }
-});
-
-
-app.whenReady()
-.then(() => {
-
-  if (process.platform === 'win32'){
-    app.setAppUserModelId(app.name);
+    Tray.getInstance()
   }
 
-  if (process.platform === "win32" || process.platform === "darwin") {
-    app.setLoginItemSettings({
-      openAtLogin: true,
-      path: app.getPath('exe')
-    })
-  }
+  app.on('will-quit', () => {
+    Shortcuts.unregister()
+  })
 
-})
-.then(() => App())
-.then(() => runningNotification())
-.then(() => Shortcuts.register())
-.then(() => console.log("[INFO][GENERAL] app running!"))
+  app.disableHardwareAcceleration()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      Window.getInstance()
+    }
+  });
+
+
+  app.whenReady()
+  .then(() => {
+
+    if (process.platform === 'win32'){
+      app.setAppUserModelId(app.name);
+    }
+
+    if (process.platform === "win32" || process.platform === "darwin") {
+      app.setLoginItemSettings({
+        openAtLogin: true,
+        path: app.getPath('exe')
+      })
+    }
+
+  })
+  .then(() => App())
+  .then(() => runningNotification())
+  .then(() => Shortcuts.register())
+  .then(() => console.log("[INFO][GENERAL] app running!"))
+
+}
