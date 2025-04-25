fd33e4d0c157dbec60f4ae292fce9104d1e8803e
Erick Rodrigues <erick.rodrigues@ifood.com.br>
2025-04-24 23:48:20 -0300

    fix: gpt model not defined

--- electron/store/config.ts
+++ electron/store/config.ts
@@ -7,3 +7,5 @@ export const store = new Store({
     // encryptionKey: '8s84d1fas8d54f2a95df2as8d5f2asd85f2asd85fas2d8fas12',
     watch: true
 });
+
+if (process.env.VITE_DEV_SERVER_URL) console.log(store.path)
