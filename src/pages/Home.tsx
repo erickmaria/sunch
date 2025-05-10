1d52ed72f7ea538374ec6b5c1f7b84020c8b634d
Erick Maria <erickmaria08@gmail.com>
2025-05-10 16:28:44 -0300

    chore: improment slash commands

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -17,6 +17,11 @@ export default function Home() {
   const [settings, setSettings] = useState(false)
   const { awaiting, makeQuestion } = useGetAnswer({})
 
+  SlashCommands.add('/clear', function (setValue: React.Dispatch<React.SetStateAction<Array<string>>>, setInput: React.Dispatch<React.SetStateAction<string>>) {
+    setValue([])
+    setInput('')
+  })
+
   const resizeTextarea = () => {
 
     if (textareaRef.current) {
@@ -41,9 +46,9 @@ export default function Home() {
 
       if (input.length == 0) return
 
-      if (SlashCommands.validate('/clear', input)) {
-        setValues([])
-        setInput('')
+      let callback = SlashCommands.execute(input)
+      if (callback !== undefined) {
+        callback(setValues, setInput)
         return
       }
 
@@ -72,61 +77,61 @@ export default function Home() {
   return (
     <>
       <div id="main-container" className={awaiting || settings ? 'bg-background rounded-t-xl' : 'bg-background rounded-xl'}>
-      <div className='border rounded-xl flex flex-col justify-center min-h-[90px] p-2'>
-        <div className='flex flex-row'>
-          <div className='p-2 draggable'>
-            <img
-              
-              style={{ width: 22, height: 22 }} src={sunchIcon} alt="sunch icon"
-            />
-          </div>
-          <div className='w-[94%]'>
-            <textarea
-              className='w-full  min-h-fit max-h-[200px] border rounded-md bg-secondary p-2 resize-none laceholder:opacity-50 placeholder:text-foreground'
-              ref={textareaRef}
-              autoFocus
-              onFocus={() => setSettings(false)}
-              name='search'
-              id='search'
-              rows={1}
-              placeholder='Ask something'
-              value={input}
-              onChange={e => setInput(e.target.value)}
-              onKeyDown={e => keyDownHandler(e)}
-            ></textarea>
-          </div>
-          <div className='p-2 '>
-            <MoreVertical size={24}
-              className='cursor-pointer'
-              onClick={() => (settings ? setSettings(false) : setSettings(true))}
-            />
-          </div>
-        </div>
-        <div className='flex flex-row justify-between w-[99%] bottom-1 h-[40px]'>
-          <div className='flex'>
-            <div className='flex items-center w-fit h-fit hover:bg-secondary rounded-md p-2'>
-              <File size={20} />
-              <p className='pl-1 text-[14px]'>Attach</p>
-            </div>
-            <div className='flex items-center w-fit h-fit hover:bg-secondary rounded-md p-2'>
-              <Globe size={20} />
-              <p className='pl-1 text-[14px]'>Search</p>
-            </div>
-          </div>
-          <div className='flex'>
-            <div className='flex items-center w-fit h-fit hover:bg-secondary rounded-md p-2'>
-              <Microphone
-                className=''
-                lang='pt-BR'
-                onErrorMessage={setValues}
-                onTranscriptData={setInput}
+        <div className='border rounded-xl flex flex-col justify-center min-h-[90px] p-2'>
+          <div className='flex flex-row'>
+            <div className='p-2 draggable'>
+              <img
+
+                style={{ width: 22, height: 22 }} src={sunchIcon} alt="sunch icon"
               />
-              <p className='pl-1 text-[14px]'>Voice</p>
+            </div>
+            <div className='w-[94%]'>
+              <textarea
+                className='w-full  min-h-fit max-h-[200px] border rounded-md bg-secondary p-2 resize-none laceholder:opacity-50 placeholder:text-foreground'
+                ref={textareaRef}
+                autoFocus
+                onFocus={() => setSettings(false)}
+                name='search'
+                id='search'
+                rows={1}
+                placeholder='Ask something'
+                value={input}
+                onChange={e => setInput(e.target.value)}
+                onKeyDown={e => keyDownHandler(e)}
+              ></textarea>
+            </div>
+            <div className='p-2 '>
+              <MoreVertical size={24}
+                className='cursor-pointer'
+                onClick={() => (settings ? setSettings(false) : setSettings(true))}
+              />
+            </div>
+          </div>
+          <div className='flex flex-row justify-between w-[99%] bottom-1 h-[40px]'>
+            <div className='flex'>
+              <div className='flex items-center w-fit h-fit hover:bg-secondary rounded-md p-2'>
+                <File size={20} />
+                <p className='pl-1 text-[14px]'>Attach</p>
+              </div>
+              <div className='flex items-center w-fit h-fit hover:bg-secondary rounded-md p-2'>
+                <Globe size={20} />
+                <p className='pl-1 text-[14px]'>Search</p>
+              </div>
+            </div>
+            <div className='flex'>
+              <div className='flex items-center w-fit h-fit hover:bg-secondary rounded-md p-2'>
+                <Microphone
+                  className=''
+                  lang='pt-BR'
+                  onErrorMessage={setValues}
+                  onTranscriptData={setInput}
+                />
+                <p className='pl-1 text-[14px]'>Voice</p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      {awaiting ? !settings && <Loading /> : !settings && <Result contents={values} />}
+        {awaiting ? !settings && <Loading /> : !settings && <Result contents={values} />}
       </div>
       {settings && <SearchSettings setSettings={setSettings} />}
     </>
