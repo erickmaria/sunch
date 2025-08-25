a5d0211aba68f31e5ded94f80b5f1d3d61f25850
Erick Maria <erickmaria08@gmail.com>
2025-08-25 17:52:43 -0300

    chore: enable devtools

--- electron/ui/windows/home.ts
+++ electron/ui/windows/home.ts
@@ -47,6 +47,7 @@ class Window {
         win.webContents.openDevTools({ mode: 'detach' })
       }
     } else {
+      console.log("DevTools: enabled")
       win.webContents.openDevTools({ mode: 'detach' })
       win.loadFile('dist/index.html')
     }
